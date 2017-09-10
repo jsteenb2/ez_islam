672f1df6830ff2400638af09eea3870598b771b2
jsteenb2 <jonathansteenbergen@gmail.com>
2017-09-10 15:51:42 -0500

    add quran.db generator that includes specificying full path
    using PWD to get quran.db from current dir in netlify

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 
 	"github.com/jsteenb2/ez_islam/builder"
 	"github.com/jsteenb2/quran"
+	"log"
 )
 
 var BaseURL = os.Getenv("base")
 
 func main() {
-	db := quran.GetQuranDB()
+	db := quran.GetQuranDB(getDBPath())
 	builder.GenerateQuran(db, BaseURL)
+}
+
+func getDBPath() string {
+	dbPath, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dbPath + "/quran.db"
 }
